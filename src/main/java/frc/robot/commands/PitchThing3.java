8d399ec2d03dd43bb67b5b800b547f458897e232
NordicStorm <nordicstorm3018@saintpeterrobotics.org>
2023-07-19 19:54:30 -0500

    Day 1 - Arm and PitchThing1-3

---
+++ src/main/java/frc/robot/commands/PitchThing3.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class PitchThing3 extends CommandBase {
+    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+
+    private double power;
+
+    /**
+     * Creates a new ExampleCommand.
+     *
+     * @param subsystem The subsystem used by this command.
+     */
+    public PitchThing3(double power) {
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(RobotContainer.arm);
+        this.power=power;
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        RobotContainer.arm.setRawPitchPower(power);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        RobotContainer.arm.setRawPitchPower(0);
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
